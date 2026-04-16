c34592bba9e2f8b1ad595bbb5b0f526baf91ab8c
VaddadiVasavi13 <238w1a12c5@vrsec.ac.in>
2026-04-16 09:59:02 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+           <Route path="/" element>  </Route>
+          <Route path="/register" element={<Register/>}></Route>
+          </Routes>
+      
+      </BrowserRouter>
+    </div>
+  )
+}
