71ea940917d745bd2ca621afab6553748aaea1e7
VaddadiVasavi13 <238w1a12c5@vrsec.ac.in>
2026-04-17 21:44:23 +0530

    changes

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    //baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-ecommerece-2rco.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
